b440839797d9ad9e3580b1b39749485b97e5efc7
Arthur-1a <0000111509578XSP@al.educacao.sp.gov.br>
2024-08-15 15:38:33 -0300

    adicionando css e js

---
+++ sketh.js
@@ -0,0 +1,62 @@
+Skip to Play Sketch
+Cookies
+The p5.js Editor uses cookies. Some are essential to the website functionality and allow you to manage an account and preferences. Others are not essential—they are used for analytics and allow us to learn more about our community. We never sell this data or use it for advertising. You can decide which cookies you would like to allow, and learn more in our Privacy Policy.
+
+
+Allow All
+
+Allow Essential
+File
+Edit
+Sketch
+Help
+English
+Hello, aarthur1c!
+
+
+
+Auto-refresh
+
+Extreme mantis
+
+Sketch Files
+
+
+i
+nde
+x
+.html
+
+s
+ketc
+h
+.js
+
+
+s
+tyl
+e
+.css
+
+sketch.js
+1
+function setup() {
+2
+  createCanvas(400, 400);
+3
+}
+4
+​
+5
+function draw() {
+6
+  background(220);
+7
+}
+There are no lint messages
+Current lineline 16
+
+Console
+Clear
+
+Preview
